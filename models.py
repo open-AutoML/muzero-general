d8859d526f63ce7fab1b43f7b925f1f25777838a
amitfridman <amitf41@gmail.com>
2021-05-11 09:45:25 +0300

    Update models.py

--- models.py
+++ models.py
@@ -6,21 +6,7 @@ import torch
 
 class MuZeroNetwork:
     def __new__(cls, config):
-        if config.network == "fullyconnected":
-            return MuZeroFullyConnectedNetwork(
-                config.observation_shape,
-                config.stacked_observations,
-                len(config.action_space),
-                config.encoding_size,
-                config.fc_reward_layers,
-                config.fc_value_layers,
-                config.fc_policy_layers,
-                config.fc_representation_layers,
-                config.fc_dynamics_layers,
-                config.support_size,
-            )
-        elif config.network == "resnet":
-            return MuZeroResidualNetwork(
+        return MuZeroResidualNetwork(
                 config.observation_shape,
                 config.stacked_observations,
                 len(config.action_space),
@@ -34,10 +20,6 @@ class MuZeroNetwork:
                 config.resnet_fc_policy_layers,
                 config.support_size,
                 config.downsample,
-            )
-        else:
-            raise NotImplementedError(
-                'The network parameter should be "fullyconnected" or "resnet".'
             )
 
 
